63c3f3534c7cbca23e8c2b8978ed28e54a8c7368
JashBon <goamail@163.com>
2024-03-27 20:36:57 +0800

    轮播块上传

--- front/src/modules/dashboard/views/index.vue
+++ front/src/modules/dashboard/views/index.vue
@@ -28,17 +28,25 @@
     <div class="mt-6">
       <div class="flex flex-wrap">
         <div class="lg:flex-8 lg:max-w-2/3 w-full lg:mb-0 lg:pr-3.5">
-          <div class="baberrage">
-            <vue-danmaku class="danmaku" ref="danmakuRef" v-model:danmus="danmus" useSlot loop :speeds="100" :top="50" :right="50" :fontSize="50"
-              :randomChannel=true>
-              <template v-slot:dm="{ danmu }">
-                <div class="danmaku-name">{{ danmu }}</div>
-              </template>
-            </vue-danmaku>
+          <div class="carousel">
+            <el-carousel height="400px">
+              <el-carousel-item v-for="item in danmus" :key="item">
+                <h3 class="carousel-text" text="2xl"
+                  :style="[{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }, { color: colors[Math.round(Math.random() * 20)] }]">
+                  {{ item }}</h3>
+              </el-carousel-item>
+            </el-carousel>
           </div>
         </div>
         <div class="lg:flex-4 lg:max-w-1/3 w-full lg:pl-3.5">
-          <SocialTrafficTable />
+          <div class="baberrage">
+            <vue-danmaku class="danmaku" ref="danmakuRef" v-model:danmus="danmus" useSlot loop :speeds="100" :top="50"
+              :right="50" :fontSize="50" :randomChannel=true>
+              <template v-slot:dm="{ danmu }">
+                <div :style="{ color: colors[Math.round(Math.random() * 20)] }">{{ danmu }}</div>
+              </template>
+            </vue-danmaku>
+          </div>
         </div>
       </div>
     </div>
@@ -83,6 +91,28 @@ const danmus = ref([
   '每天都要保持好心情，生活才会更美好。'
 ])
 
+const colors = ref([
+  "#ffb980",
+  "#2ec7c9",
+  "#5ab1ef",
+  "#b6a2de",
+  "#d87a80",
+  "#8d98b3",
+  "#e5cf0d",
+  "#97b552",
+  "#95706d",
+  "#dc69aa",
+  "#07a2a4",
+  "#9a7fd1",
+  "#588dd5",
+  "#f5994e",
+  "#c05050",
+  "#59678c",
+  "#c9ab00",
+  "#7eb00a",
+  "#6f5553",
+  "#c14089",
+  "#409eff",])
 </script>
 
 <style lang="less" scoped>
@@ -90,22 +120,36 @@ const danmus = ref([
   width: 100%;
   height: 400px;
   background: #ffffff;
+
   .danmaku {
     width: 100%;
     height: 100%;
 
-    &-name {
-      height: 100%;
-      background: #f0f0f0;
-      margin-right: 50px;
-      color: #000000;
-      --fa-font-solid: normal 900 1em / 1 "Font Awesome 6 Solid";
-      --fa-font-regular: normal 400 1em / 1 "Font Awesome 6 Regular";
-      --fa-font-light: normal 300 1em / 1 "Font Awesome 6 Light";
-      --fa-font-thin: normal 100 1em / 1 "Font Awesome 6 Thin";
-      --fa-font-duotone: normal 900 1em / 1 "Font Awesome 6 Duotone";
-      --fa-font-brands: normal 400 1em / 1 "Font Awesome 6 Brands"
-    }
+    &-name {}
   }
+}
+
+.carousel {
+  display: block;
+  text-align: center;
+  width: 100%;
+  height: 400px;
+
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
