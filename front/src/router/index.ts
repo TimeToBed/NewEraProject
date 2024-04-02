796e25ee5fc81b93cbc8de702e410061eec81e85
Renalyn <0124renalyn@gmail.com>
2024-04-02 21:36:46 +0800

    111

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,5 +1,5 @@
 import useStore from 'store'
-import { createRouter, createWebHistory, Router } from 'vue-router'
+import { createRouter, createWebHistory, Router, RouteLocationNormalized, NavigationGuardNext } from 'vue-router'
 import routes from './routes'
 
 const router: Router = createRouter({
@@ -7,9 +7,17 @@ const router: Router = createRouter({
   routes,
 })
 
-router.beforeEach(() => {
+router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
   const store = useStore()
   store.dashboard.setIsSBOpen(false)
+
+  if (window.innerWidth <= 640 && to.name == 'DesktopView') {
+    next({ name: 'MobileView' })
+  } else if (window.innerWidth > 640 && to.name == 'MobileView') {
+    next({ name: 'DesktopView' })
+  } else {
+    next()
+  }
 })
 
 export default router
